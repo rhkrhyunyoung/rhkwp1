e8baee4c02c211f815d86a8326db77aa60b0d7a6
rhkrhyunyoung <cindy2084@kookmin.ac.kr>
2025-05-15 14:58:32 +0900

    수정

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main() {
-    std::string input = "I love KiWi !!!33";
+    std::string input = "I like KiWi !!!33";
     std::cout << "Original: " << input << std::endl;
     std::cout << "Upper: " << Utils::String::StringHelper::toUpperCase(input) << std::endl;
     std::cout << "Lower: " << Utils::String::StringHelper::toLowerCase(input) << std::endl;
